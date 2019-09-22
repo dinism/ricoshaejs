1daea6da739e740348bb38329f2b409173bdac8e
Christopher M. Richter <chris@ricoshae.com.au>
2019-09-23 08:44:26 +1000

    initial code

---
+++ README.md
@@ -0,0 +1,5 @@
+# ricoshaejs
+
+This is a Moodle 3.2+ Activities plugin that allows you to create JavaScript activities in Moodle.
+
+For more information https://ricoshae.com.au/ricoshaejs
---
+++ lang/en/ricoshaejs.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for ricoshaejs
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_ricoshaejs
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Ricoshae Interactive';
+$string['modulenameplural'] = 'Ricoshae Interactives';
+$string['modulename_help'] = 'Use the Ricoshae Interactive module for... | The ricoshaejs module allows...';
+$string['ricoshaejs:addinstance'] = 'Add a new Ricoshae Interactive';
+$string['ricoshaejs:submit'] = 'Submit Ricoshae Interactive';
+$string['ricoshaejs:view'] = 'View Ricoshae Interactive';
+$string['ricoshaejsfieldset'] = 'Custom example fieldset';
+$string['ricoshaejsname'] = 'Ricoshae Interactive name';
+$string['ricoshaejsname_help'] = 'This is the content of the help tooltip associated with the Ricoshae Interactive field. Markdown syntax is supported.';
+$string['ricoshaejs'] = 'ricoshaejs';
+$string['pluginadministration'] = 'Ricoshae Interactive administration';
+$string['pluginname'] = 'Ricoshae Interactive';
+$string['javascript'] = 'Javascript';
+$string['css'] = 'CSS';
+$string['html'] = 'HTML';
+$string['javascriptcode'] = 'Javascript Code';
+$string['csscode'] = 'CSS Code';
+$string['htmlcode'] = 'HTML Code';
